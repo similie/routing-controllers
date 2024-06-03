5b875d3fab45828814636d4ea498ad1ab057a802
Adam Smith <adam.smith@similie.org>
2024-06-03 08:51:31 +0800

    pulled build files to root

---
+++ types/CurrentUserChecker.d.ts
@@ -0,0 +1,5 @@
+import { Action } from './Action';
+/**
+ * Special function used to get currently authorized user.
+ */
+export type CurrentUserChecker = <User = any>(action: Action) => Promise<User | null> | User | null;
