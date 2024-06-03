1edf14568214535afba352555afc1e6c6bafbbee
Adam Smith <adam.smith@similie.org>
2024-06-03 09:13:06 +0800

    chore: removed production files

--- types/CurrentUserChecker.d.ts
+++
@@ -1,5 +0,0 @@
-import { Action } from './Action';
-/**
- * Special function used to get currently authorized user.
- */
-export type CurrentUserChecker = <User = any>(action: Action) => Promise<User | null> | User | null;
